0730f3f543e2c47bfca911e04c344cdeae227eaf
Kishor <kaushalyekishor1232gmail.com>
2020-07-13 19:30:26 +0530

    emp wage for month code added

--- empWage.sh
+++ empWage.sh
@@ -168,3 +168,32 @@ case $empCheck in
 esac
 salary=$(($empHours * $empRatePerHours));
 echo $salary
+
+
+
+
+##emp wage for month code added
+#!/bin/bash -x
+isPartTime=1
+isFullTime=2
+empRatePerHours=20
+totalSalary=0
+empCheck=$((RANDOM%3))
+numberOfDays=20
+for (( day=1;day<=$numberOfDays;day++ ))
+do
+case $empCheck in
+	$isFullTime)
+		empHours=8
+		;;
+	$isPartTime)
+		empHours=4
+		;;
+	*)
+		empHours=0
+		;;
+esac
+salary=$(($empHours * $empRatePerHours))
+totalSalary=$(($totalSalary + $salary))
+done
+echo $totalSalary
