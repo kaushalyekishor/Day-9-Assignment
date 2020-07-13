90a7e42121ddb5d9f0777976c470d46fc58f97d0
Kishor <kaushalyekishor1232gmail.com>
2020-07-13 19:18:11 +0530

    emp Attendance code added

--- empWage.sh
+++ empWage.sh
@@ -130,3 +130,17 @@ do
 done
 totalSalary=$(($totalEmpHrs * $empRatePerHour))
 echo ${dailyWage[@]}
+
+
+
+
+##emp Attendance code added
+#!/bin/bash -x
+isPresent=1
+randomCheck=$((RANDOM%2))
+if [ $randomCheck -eq $isPresent ]
+then
+echo "employee is present"
+else
+echo "employee is absent"
+fi
