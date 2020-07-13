313eb536b4da3e9380b54fe55cd2503c1353c885
Kishor <kaushalyekishor1232gmail.com>
2020-07-13 19:48:36 +0530

    part time emp wage code added

--- empWage.sh
+++ empWage.sh
@@ -268,3 +268,49 @@ salary=$(($empHours * $empRatePerHours))
 totalSalary=$(($totalSalary + $salary))
 done
 echo $totalSalary
+
+
+
+
+##part time emp wage code added
+#!/bin/bash -x
+isPartTime=1;
+maxHrsInMonth=10;
+empRatePerHour=20;
+totalEmpHrs=0;
+
+numberOfWorkingDays=20;
+totalWorkingDays=0;
+function getEmpWage()
+{
+        local empWage=$1;
+        empHours=$(( $empWage * $empRatePerHour ))
+        echo $(($empHours))
+}
+
+function getWorkingHours()
+{
+        local empCheck=$1
+        case $empCheck in
+        $isPartTime)
+                empHours=8
+                ;;
+        *)
+                empHours=0
+                ;;
+esac
+echo $empHours
+}
+while [[ $totalEmpHrs -lt $maxHrsInMonth &&
+                                $totalWorkingDays -lt $numberOfWorkingDays ]]
+do
+        ((totalWorkingDays++))
+        #empCheck=$((RANDOM%3))
+        empCheck=1
+        empHours="$(getWorkingHours $empCheck)"
+        totalEmpHrs=$(( $totalEmpHrs + $empHours ))
+        dailyWage[$totalWorkingDays]=$(getEmpWage $empHours)
+done
+totalSalary=$(($totalEmpHrs * $empRatePerHour))
+echo ${dailyWage[@]}
+
