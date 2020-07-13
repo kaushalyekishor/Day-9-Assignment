871adc0d2a15e1cd5d1d173382f77a958da75a4c
Kishor <kaushalyekishor1232gmail.com>
2020-07-13 19:43:10 +0530

    emp wage working Hours code added

--- empWage.sh
+++ empWage.sh
@@ -239,3 +239,32 @@ empHours="$(getWorkingHours $empCheck)"
 totalEmpHours=$(($totalEmpHours + $empHours))
 done
 totalSalary=$(($totalEmpHours * $empRatePerHours))
+
+
+
+
+##emp wage working Hours code added
+#!/bin/bash -x
+isPartTime=1
+isFullTime=2
+empRatePerHours=20
+totalSalary=0
+empCheck=$((RANDOM%3))
+numberOfDays=20
+for (( day=1;day<=$numberOfDays;day++ ))
+do
+case $empCheck in
+	$isFullTime)
+		empHours=8
+		;;
+	$isPartTime)
+		empHours=4
+		;;
+	*)
+		empHours=0
+		;;
+esac
+salary=$(($empHours * $empRatePerHours))
+totalSalary=$(($totalSalary + $salary))
+done
+echo $totalSalary
